8d65fc6582c410444bb1f892d1451826f3919c30
Ian Wong <wong.iancw@gmail.com>
2016-08-07 11:12:30 +1000

    Initial commit

---
+++ src/artistic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# COMP9517 16s2 Assignment 1: Digital Art
+# This program is designed to achieve the following tasks:
+#	- Open and read image files
+#	- Perform simple mathematical operations on images
+#	- Perform filtering using convolution masks
+#	- Perform image manipulation and adjustment
+# Written by: Ian Wong
+# Date started: 7/8/2016
+
+# -------------------------
+# FUNCTIONS
+# -------------------------
+
+# Main program
+def main():
+
+	# Read image file
+	pass
+
+# ------------------------------------
+if (__name__ == "__main__"):
+	if len(sys.argv) < 2:
+		print "Usage: python artistic.py <image file>"
+		exit()
+
+	main()
