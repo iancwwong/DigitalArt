59191ba5756738fb03c2616d54ccf4b44cf3e628
Ian Wong <wong.iancw@gmail.com>
2016-08-09 16:22:49 +1000

    Able to achieve oil effect

--- src/artistic.py
+++ src/artistic.py
@@ -27,6 +27,10 @@ IMG_PIXEL_BLUE = 0
 IMG_PIXEL_GREEN = 1
 IMG_PIXEL_RED = 2
 
+# Mask dimensions
+MASK_HEIGHT = 9
+MASK_WIDTH = 9
+
 # -------------------------
 # FUNCTIONS
 # -------------------------
@@ -45,12 +49,12 @@ def main(image_filename):
 	cv2.imwrite("_task2.png",task2_img)
 
 	# Conduct task 3
-#	task3_img = task3(task2_img)
-#	cv2.imwrite("_task3.png", task3_img)
+	task3_img = task3(task2_img, img)
+	cv2.imwrite("_task3.png", task3_img)
 
 	# Additional effect - Gaussian Blur
-#	blurred = gaussian_blur(task3_img)
-#	cv2.imwrite("_opt.png", blurred)
+	blurred = gaussian_blur(task3_img)
+	cv2.imwrite("_opt.png", blurred)
 
 # Convert an image to greyscale
 def conv_greyscale(img):
@@ -96,7 +100,7 @@ def task2(img):
 	for i in range(0, height):
 		for j in range(0, width):
 			# Get value of pixel (at i,j) and its neighbours
-			neighbour_vals = get_neighbours(img, (3,3), (i,j))	# 3x3 mask
+			neighbour_vals = get_neighbours(img, (MASK_HEIGHT,MASK_WIDTH,), (i,j))
 			
 			# Get most frequent pixel value
 			most_freq_val = get_most_freq_val(neighbour_vals)
@@ -108,7 +112,33 @@ def task2(img):
 
 	return proc_img
 
-# Obtains the neighbouring pixel values as an array
+# Task 3: Achieves the 'oil paint' effect
+def task3(grey_img, orig_img):
+	print "Conducting task 3..."
+	proc_img = orig_img.copy()
+
+	# Obtain dimensions of image
+	height, width, depth = grey_img.shape
+	for i in range(0, height):
+		for j in range(0, width):
+
+			# Get location of neighbour pixels with equal pixel values to the one at (i,j)
+			pixel_val = grey_img.item(i,j,0)
+			neighbours = get_neighbours(grey_img, (MASK_HEIGHT, MASK_WIDTH), (i,j))
+			locs = equal_neighbour_val_locs(neighbours, pixel_val)
+
+			# Calculate average RGB intensities
+			avg_intensities = get_avg_intensities(locs, orig_img)		# Order: B,G,R
+
+			# Set new pixel value to avg intensities
+			proc_img.itemset((i,j,IMG_PIXEL_RED),avg_intensities[IMG_PIXEL_RED])	
+			proc_img.itemset((i,j,IMG_PIXEL_GREEN),avg_intensities[IMG_PIXEL_GREEN])	
+			proc_img.itemset((i,j,IMG_PIXEL_BLUE),avg_intensities[IMG_PIXEL_BLUE])
+
+	return proc_img
+
+# Obtains the neighbouring pixel values and their locations as an array
+# ie returns array of (pixel_value, loc)
 # NOTE: Assumes mask dimensions are odd, pixel_loc is valid, and img is a GREYSCALE image
 def get_neighbours(img, mask_dim, pixel_loc):
 	neighbours = []
@@ -123,23 +153,37 @@ def get_neighbours(img, mask_dim, pixel_loc):
 		if (i >= 0) and (i < img_height):			# check for height bounded
 			for j in range(pixel_y - mask_width/2, pixel_y + mask_width/2 + 1):
 				if (j >= 0) and (j < img_width):	# check for width bounded
-					neighbours.append(img.item(i,j,0))
+					neighbours.append((img.item(i,j,0), (i,j)))
 			
 	return neighbours
 	
 
-# Obtains the most frequent value in an array
+# Obtains the most frequent pixel value
+# 'values' supplied with each element as a tuple: (pixel_value, loc)
 def get_most_freq_val(values):
-	return np.bincount(values).argmax()
+	# Construct list with only pixel values
+	val_list = [value[0] for value in values]
+	return np.bincount(val_list).argmax()
 
-# Task 3: Achieves the 'oil paint' effect
-def task3(img):
-	print "Conducting task 3..."
-	proc_img = img.copy()
+# Returns locations that have pixel value equal to a specified value
+# NOTE: 'neighbours' is an array with elements as a tuple, in format: (neighbour_val, neighbour_loc)
+def equal_neighbour_val_locs(neighbours, pixel_val):
+	return [ neighbour[1] for neighbour in neighbours if neighbour[0] == pixel_val ]
 
-	# Do some processing
-
-	return proc_img
+# Returns the average RGB intensities based on particular locations in a specified image
+# NOTE: Returns an array with intensities in the order: B, G, R
+def get_avg_intensities(locs, img):
+	r = 0
+	g =0
+	b = 0
+	for loc in locs:
+		r += img.item(loc[0],loc[1],IMG_PIXEL_RED)
+		g += img.item(loc[0],loc[1],IMG_PIXEL_GREEN)
+		b += img.item(loc[0],loc[1],IMG_PIXEL_BLUE)
+	r = r/len(locs)
+	g = g/len(locs)
+	b = b/len(locs)
+	return [b,g,r]
 
 # -----------------------------------------------------------------------------------
 if (__name__ == "__main__"):
